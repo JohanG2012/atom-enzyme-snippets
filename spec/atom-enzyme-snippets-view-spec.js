5874d401bbd3fe84ccb8191a2baa67b893beec52
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-02 20:14:54 +0200

    Initial commit

---
+++ spec/atom-enzyme-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomEnzymeSnippetsView from '../lib/atom-enzyme-snippets-view';
+
+describe('AtomEnzymeSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
